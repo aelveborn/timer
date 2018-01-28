855629b6065e63bd2b8790d05488c041098a7152
Andreas Älveborn <andreas@Andreas-MacBook-Pro.local>
2018-01-28 01:10:36 +0100

    Background images from unsplash

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Timer from './timer';
 import TimeFormat from '../utils/timeFormat';
 import Storage from '../utils/storage';
+import Background from './background';
 
 class App extends Component {
 
@@ -9,7 +10,7 @@ class App extends Component {
     super(props);
     this.storage = new Storage();
     this.timeFormat = new TimeFormat();
-    this.defaultTitle = 'Hi there, whatcha doing?';
+    this.defaultTitle = 'Hi there, type a name here';
 
     let storedData = this.getStoredData();
 
@@ -67,24 +68,45 @@ class App extends Component {
 
   handleTitleBlur(event) {
     let value = event.target.value;
-
     if(value === '') {
       value = this.defaultTitle;
     }
-
     this.setTitle(value);
+  }
+
+  handleReset(event) {
+    let ts = this.currentTimestamp();
+    let data = this.storage.get();
+    data.timestamp = ts;
+    this.storage.set(data);
+    
+    this.setState({
+      seconds: 0
+    });
+
+    event.preventDefault();
   }
 
   render() {
     return (
       <div className="app">
-        <div className="title">
-          <input type="text" 
-            value={this.state.title} 
-            onChange={(event) => this.handleTitleChange(event)} 
-            onBlur={(event) => this.handleTitleBlur(event)} />
+        <div className="content">
+
+          <div className="title">
+            <input type="text" 
+              value={this.state.title} 
+              onChange={(event) => this.handleTitleChange(event)} 
+              onBlur={(event) => this.handleTitleBlur(event)} />
+          </div>
+
+          <Timer seconds={this.state.seconds} />
+
+          <div className="reset">
+            <a href="#" className="defaultButton" onClick={(event) => this.handleReset(event)}>Reset timer</a>
+          </div>
+
         </div>
-        <Timer seconds={this.state.seconds} />
+        <Background />
       </div>
     );
   }
---
+++ src/components/background.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class Timer extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.setBackground();
+  }
+
+  setBackground() {
+    var downloadingImage = new Image();
+    downloadingImage.onload = function () {
+      document.body.style.backgroundImage = 'url(' + this.src + ')';
+      document.getElementById('backgroundMask').style.transitionDuration = "2s";
+      document.getElementById('backgroundMask').style.opacity = "0";
+    };
+    downloadingImage.src = 'https://source.unsplash.com/collection/1657989/1920x1200&ts=' + Date.now();
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    document.getElementById('backgroundMask').style.transitionDuration = "0.4s";
+    document.getElementById('backgroundMask').style.opacity = "1";
+    this.setBackground();
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="backgroundControls">
+          <a href="#" className="backgroundButton" onClick={(event) => this.handleClick(event)}>
+            <i className="fa fa-picture-o" aria-hidden="true"></i>
+          </a>
+        </div>
+        <div id="backgroundMask" className="mask"></div>
+      </div>
+    )
+  }
+}
+
+export default Timer;
